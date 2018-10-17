620b7059083859787072d46b6aaa74029614149c
Mita <milosrancic94@gmail.com>
2018-10-17 18:41:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 $(function () {
+	
 	// fade in and bend h1
 	$('h1').css('display', 'none')
-	.fadeIn(1000)
-	.arctext({radius:800});
-
-
-
-
-
-
-
-
+				 .fadeIn(1000)
+	 			 .arctext({radius:800});
+	
 });
