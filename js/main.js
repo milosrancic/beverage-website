22e7a2fe05dd1471f93ad7cce0fa1e82fd72135c
Milos <milosrancic94@gmail.com>
2019-02-02 18:02:19 +0100

    deleted archtext script

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(function () {
-	
-	// fade in and bend h1
-	$('h1').css('display', 'none')
-				 .fadeIn(1000)
-	 			 .arctext({radius:800});
-	
-	
+
 // 	smooth scrolling
 	$('a').on('click', function(event) {
 	
